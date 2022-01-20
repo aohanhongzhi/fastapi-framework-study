93f2674479ef4fa805806f34f05d9cf030d0db0d
aohanhongzhi <aohanhongzhi@qq.com>
2022-01-20 22:34:29 +0800

    测试并发程度

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi import FastAPI
 from loguru import logger
 import uvicorn
@@ -7,16 +9,17 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    logger.debug("\n====>访问fastapi首页")
+    logger.debug("====>访问fastapi首页")
+    time.sleep(9)
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    logger.debug("\n====>输入参数是{},{}",item_id,q)
+    logger.debug("\n====>输入参数是{},{}", item_id, q)
     return {"item_id": item_id, "q": q}
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     logger.debug("程序开始启动")
-    uvicorn.run(app=app,host="127.0.0.1",port=8000)
+    uvicorn.run(app=app, host="127.0.0.1", port=8000)
