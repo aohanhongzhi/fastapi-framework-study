0978a5d6e3a71c0ed1ad265f2982f38d765bb3b2
aohanhongzhi <aohanhongzhi@qq.com>
2022-01-23 01:16:05 +0800

    fa

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ app = FastAPI()
 
 
 @app.get("/")
-@pysnooper.snoop() # 这个必须贴近下面方法名
+@pysnooper.snoop()  # 这个必须贴近下面方法名
 def read_root():
     logger.debug("====>访问fastapi首页")
 
     # 测试线程并发度 经过多番测试大概并发度一般是 CPU核心数+1
     time.sleep(4)
     return {"Hello": "World"}
-    
+
+
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
