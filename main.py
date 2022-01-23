795f777e98638ffc6b12a0908a0b5f3f5014fd8a
aohanhongzhi <aohanhongzhi@qq.com>
2022-01-23 10:18:43 +0800

    0.0.0.0 远程可以访问

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def read_item(item_id: int, q: str = None):
 
 if __name__ == '__main__':
     logger.debug("程序开始启动")
-    uvicorn.run(app=app, host="127.0.0.1", port=8000)
+    uvicorn.run(app=app, host="0.0.0.0", port=8000)
